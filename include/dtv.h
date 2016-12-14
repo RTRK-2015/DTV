5490ac84192ca0a277b7a663be0fa008a720fa87
authext <wolf.srb.ns@hotmail.com>
2016-12-14 10:15:54 +0100

    Add missing prefix to the structure

--- include/dtv.h
+++ include/dtv.h
@@ -9,21 +9,21 @@
 
 
 /// \brief A structure that holds the initial dtv settings.
-struct init_pr_info
+struct dtv_init_ch_info
 {
 	uint32_t freq;
-	uint32_t bw;
-	t_Module module;
-	uint16_t vpid; ///< pid of the initial video stream
-	uint16_t apid; ///< pid of the initial audio stream
-	t_StreamType vtype; ///< type of the inital video stream
-	t_StreamType atype; ///< type of the initial audio stream
-	uint32_t pr_num;
+	uint32_t bandwidth;
+	t_Module module; ///< Whether the channel uses DTB-T or DTB-T2.
+	uint16_t vpid; ///< pid of the initial video stream.
+	uint16_t apid; ///< pid of the initial audio stream.
+	t_StreamType vtype; ///< type of the inital video stream.
+	t_StreamType atype; ///< type of the initial audio stream.
+	uint32_t ch_num; ///< Channel number.
 };
 
 
 /// \brief Function that initializes internal DTV state.
-void dtv_init(struct init_pr_info init_info);
+void dtv_init(struct dtv_init_ch_info init_info);
 /// \brief Returns a pointer to the array of program (channel) numbers.
 const uint16_t* dtv_get_channels();
 /// \brief Tries to switch to the desired channel.
