ce92f576edbdfd0c1b0e7d1b915cc63308af2fa8
authext <wolf.srb.ns@hotmail.com>
2016-12-14 22:11:12 +0100

    Add code to extract channel number from configuration

--- src/config.c
+++ src/config.c
@@ -10,11 +10,13 @@ static const uint32_t NO_FREQUENCY = UINT32_C(0xFFFFFFFF);
 static const uint32_t NO_BANDWIDTH = UINT32_C(0xFFFFFFFF);
 static const uint16_t NO_VIDEO_PID = UINT16_C(0xFFFF);
 static const uint16_t NO_AUDIO_PID = UINT16_C(0xFFFF);
+static const uint16_t NO_CH_NUM    = UINT16_C(0xFFFF);
 
 static const uint32_t DEFAULT_FREQUENCY = UINT32_C(754000000);
 static const uint32_t DEFAULT_BANDWIDTH = UINT32_C(8);
 static const uint16_t DEFAULT_VIDEO_PID = UINT16_C(101);
 static const uint16_t DEFAULT_AUDIO_PID = UINT16_C(103);
+static const uint16_t DEFAULT_CH_NUM    = UINT16_C(490);
 
 
 #define MAKE_GETTER(TYPE, NAME, NOT_FOUND, CONVERSION) \
@@ -39,6 +41,7 @@ MAKE_GETTER(uint32_t, frequency, NO_FREQUENCY, "%"SCNu32);
 MAKE_GETTER(uint32_t, bandwidth, NO_BANDWIDTH, "%"SCNu32);
 MAKE_GETTER(uint16_t, video_pid, NO_VIDEO_PID, "%"SCNu16);
 MAKE_GETTER(uint16_t, audio_pid, NO_AUDIO_PID, "%"SCNu16);
+MAKE_GETTER(uint16_t, ch_num, NO_CH_NUM, "%"SCNu16);
 
 
 struct config_init_ch_info config_get_init_ch_info(FILE *f)
@@ -56,6 +59,9 @@ struct config_init_ch_info config_get_init_ch_info(FILE *f)
 
     uint16_t audio_pid = get_audio_pid(f);
     init_info.apid = (audio_pid == NO_AUDIO_PID)? DEFAULT_AUDIO_PID : audio_pid;
+
+    uint16_t ch_num = get_ch_num(f);
+    init_info.ch_num = (ch_num == NO_CH_NUM)? DEFAULT_CH_NUM : ch_num;
 
     return info;
 }
