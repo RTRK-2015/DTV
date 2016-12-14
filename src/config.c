3fdb743b5c0cf177a3dbdfa841302c9cc9a91c90
authext <wolf.srb.ns@hotmail.com>
2016-12-14 22:14:57 +0100

    Add code to extract module info from configuration

--- src/config.c
+++ src/config.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include "tdp_api.h"
 
 
 #define BUF_SIZE 100
@@ -11,12 +12,14 @@ static const uint32_t NO_BANDWIDTH = UINT32_C(0xFFFFFFFF);
 static const uint16_t NO_VIDEO_PID = UINT16_C(0xFFFF);
 static const uint16_t NO_AUDIO_PID = UINT16_C(0xFFFF);
 static const uint16_t NO_CH_NUM    = UINT16_C(0xFFFF);
+static const int      NO_MODULE    = -1;
 
 static const uint32_t DEFAULT_FREQUENCY = UINT32_C(754000000);
 static const uint32_t DEFAULT_BANDWIDTH = UINT32_C(8);
 static const uint16_t DEFAULT_VIDEO_PID = UINT16_C(101);
 static const uint16_t DEFAULT_AUDIO_PID = UINT16_C(103);
 static const uint16_t DEFAULT_CH_NUM    = UINT16_C(490);
+static const int      DEFAULT_MODULE    = DVB_T;
 
 
 #define MAKE_GETTER(TYPE, NAME, NOT_FOUND, CONVERSION) \
@@ -42,6 +45,7 @@ MAKE_GETTER(uint32_t, bandwidth, NO_BANDWIDTH, "%"SCNu32);
 MAKE_GETTER(uint16_t, video_pid, NO_VIDEO_PID, "%"SCNu16);
 MAKE_GETTER(uint16_t, audio_pid, NO_AUDIO_PID, "%"SCNu16);
 MAKE_GETTER(uint16_t, ch_num, NO_CH_NUM, "%"SCNu16);
+MAKE_GETTER(int,      module, NO_MODULE, "%d");
 
 
 struct config_init_ch_info config_get_init_ch_info(FILE *f)
@@ -62,6 +66,9 @@ struct config_init_ch_info config_get_init_ch_info(FILE *f)
 
     uint16_t ch_num = get_ch_num(f);
     init_info.ch_num = (ch_num == NO_CH_NUM)? DEFAULT_CH_NUM : ch_num;
+
+    int module = get_module(f);
+    init_info.module = (module == NO_MODULE)? DEFAULT_MODULE : module;
 
     return info;
 }
