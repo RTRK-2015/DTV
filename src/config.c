b589f31b52910ce5f43e7abe11f430f65522efe3
authext <wolf.srb.ns@hotmail.com>
2016-12-14 22:03:48 +0100

    Add code to extract audio PID from initial configuration

--- src/config.c
+++ src/config.c
@@ -9,10 +9,12 @@
 static const uint32_t NO_FREQUENCY = UINT32_C(0xFFFFFFFF);
 static const uint32_t NO_BANDWIDTH = UINT32_C(0xFFFFFFFF);
 static const uint16_t NO_VIDEO_PID = UINT16_C(0xFFFF);
+static const uint16_t NO_AUDIO_PID = UINT16_C(0xFFFF);
 
 static const uint32_t DEFAULT_FREQUENCY = UINT32_C(754000000);
 static const uint32_t DEFAULT_BANDWIDTH = UINT32_C(8);
 static const uint16_t DEFAULT_VIDEO_PID = UINT16_C(101);
+static const uint16_t DEFAULT_AUDIO_PID = UINT16_C(103);
 
 
 #define MAKE_GETTER(TYPE, NAME, NOT_FOUND, CONVERSION) \
@@ -36,6 +38,7 @@ static TYPE get_##NAME(FILE *f) \
 MAKE_GETTER(uint32_t, frequency, NO_FREQUENCY, "%"SCNu32);
 MAKE_GETTER(uint32_t, bandwidth, NO_BANDWIDTH, "%"SCNu32);
 MAKE_GETTER(uint16_t, video_pid, NO_VIDEO_PID, "%"SCNu16);
+MAKE_GETTER(uint16_t, audio_pid, NO_AUDIO_PID, "%"SCNu16);
 
 
 struct config_init_ch_info config_get_init_ch_info(FILE *f)
@@ -50,6 +53,9 @@ struct config_init_ch_info config_get_init_ch_info(FILE *f)
 
     uint16_t video_pid = get_video_pid(f);
     init_info.vpid = (video_pid == NO_VIDEO_PID)? DEFAULT_VIDEO_PID : video_pid;
+
+    uint16_t audio_pid = get_audio_pid(f);
+    init_info.apid = (audio_pid == NO_AUDIO_PID)? DEFAULT_AUDIO_PID : audio_pid;
 
     return info;
 }
